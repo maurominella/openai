47c593c131f668b80f237132335956532ede4efa
Mauro Minella <mauro.minella@microsoft.com>
2024-03-27 23:28:24 +0100

    autogen

---
+++ autogen/coding/.ipynb_checkpoints/get_current_date-checkpoint.py
@@ -0,0 +1,8 @@
+# filename: get_current_date.py
+from datetime import datetime
+
+# Get the current date
+current_date = datetime.now()
+
+# Print the current date in the format YYYY-MM-DD
+print(current_date.strftime('%Y-%m-%d'))
