b6e363fddf3e42cca3aa58935e671db22ba55aa5
Mauro Minella <mauro.minella@microsoft.com>
2024-03-30 09:20:32 +0100

    autogen

--- autogen/coding/get_current_date.py
+++
@@ -1,8 +0,0 @@
-# filename: get_current_date.py
-import datetime
-
-# Get the current date
-current_date = datetime.date.today()
-
-# Print the current date
-print(f"Today's date is: {current_date}")
--- coding/get_current_date.py
+++
@@ -1,9 +0,0 @@
-# filename: get_current_date.py
-
-from datetime import datetime
-
-# Get the current date
-current_date = datetime.now()
-
-# Print the current date in YYYY-MM-DD format
-print(current_date.strftime('%Y-%m-%d'))
