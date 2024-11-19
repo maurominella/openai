d645134deb035bb890cd1db5a59f60cc88a1a166
Mauro Minella <mauro.minella@mngenv789911.onmicrosoft.com>
2024-11-19 08:05:38 +0100

    withoutconfig

--- semantic_kernel/function_calling/dotnet/obj/Debug/net7.0/dotnet.AssemblyInfo.cs
+++ semantic_kernel/function_calling/dotnet/obj/Debug/net7.0/dotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f798d136579c45742d5d26b8fa24eec713ddadd2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73cf49172298e8830d8a7bfe7635f6c6b5068092")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
