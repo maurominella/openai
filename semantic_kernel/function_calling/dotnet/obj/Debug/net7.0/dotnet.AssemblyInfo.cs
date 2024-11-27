bf167646329451aa3371e0bb9c917994fafb884a
Mauro Minella <mauro.minella@microsoft.com>
2024-11-27 14:33:30 +0100

    eventhubs

--- semantic_kernel/function_calling/dotnet/obj/Debug/net7.0/dotnet.AssemblyInfo.cs
+++ semantic_kernel/function_calling/dotnet/obj/Debug/net7.0/dotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73cf49172298e8830d8a7bfe7635f6c6b5068092")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f776605fd4b800bf92edfcba9cb3a9e86282a672")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
